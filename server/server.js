265e8ff489c2c75cb76ddf28cc8323802b7ac249
Deepak4617 <dk4803382@gmail.com>
2026-03-11 16:50:28 +0530

    server file chages

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // ✅ VERY IMPORTANT — allow preflight
-app.options("/*", cors(corsOptions));
+// app.options("/*", cors(corsOptions));
 
 
 app.use(express.json());
