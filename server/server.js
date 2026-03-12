4d625d8fbfdb5b6574685a1f4ddd77943b4982dd
Deepak4617 <dk4803382@gmail.com>
2026-03-12 15:51:07 +0530

    chang

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.use(errorMiddleware);
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
