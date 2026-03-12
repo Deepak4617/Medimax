a16aeb02f0e115b5105ebb0beb725b97ac8219ab
Deepak4617 <dk4803382@gmail.com>
2026-03-12 16:24:44 +0530

    change

--- server/server.js
+++ server/server.js
@@ -42,12 +42,15 @@ app.use(errorMiddleware);
 // ================= SERVE REACT =================
 
 // Serve React build
+const path = require("path");
+
+// Serve React build
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.use((req, res) => {
+// React Router fix (important)
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
-
 // ================= SERVER =================
 
 const PORT = process.env.PORT || 5000;
